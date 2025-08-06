6a1409ff82e87a6a40217f762937e4abc96bbf04
Venky <pvenky985@gmail.com>
2025-08-06 10:01:49 +0530

    Update index.js

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -24,7 +24,7 @@ const Projects = ()=>(
                 <img src="https://wpassets.adda247.com/wp-content/uploads/multisite/sites/5/2023/12/07105923/gemini-e1701926977555.jpg" alt="Image"
                 className="project-img "/>
                 <h1 className="-projectcad-head animation-container">Gemini Clone AI</h1>
-                <p className="-projectcad-pera animation-container"> Intelligent chatbot with natural language processing, sentiment analysis, and integration with multiple platforms.</p>
+                <p className="-projectcad-pera animation-container">  AI chatbot using NLP and sentiment analysis for natural, human-like conversations across platforms.</p>
                 <div className="poject-button-container animation-container">
                     <a className="projectcad-giigub-button animation-container"
                     href="https://github.com/venkatesh3553/AI-Chatbot.git" target="_blank"
@@ -33,12 +33,12 @@ const Projects = ()=>(
                     href="https://ai-chatbot-chi-ashy.vercel.app/" target="_blank"
                     >Live Demo</a>
                 </div>   
-         </div>
+            </div>
             <div className="project-card animation-div">
                 <img src="https://png.pngtree.com/thumb_back/fh260/background/20230704/pngtree-golden-calculator-icon-on-matte-plate-3d-rendered-social-media-symbol-image_3747030.jpg" alt="Image"
                 className="project-img "/>
                 <h1 className="-projectcad-head animation-container">Calculator</h1>
-                <p className="-projectcad-pera animation-container"> Full-stack e-commerce solution with payment integration, inventory management, and admin dashboard.</p>
+                <p className="-projectcad-pera animation-container">  A clean React calculator handling basic arithmetic with responsive design and real-time result updates.</p>
                 <div className="poject-button-container animation-container">
                     <a className="projectcad-giigub-button animation-container"
                     href="https://github.com/venkatesh3553/Calculator.git" target="_blank"
@@ -52,7 +52,7 @@ const Projects = ()=>(
                 <img src="https://png.pngtree.com/thumb_back/fh260/background/20230613/pngtree-professional-resume-templates-that-you-can-customize-image_2974655.jpg" alt="Image"
                 className="project-img"/>
                 <h1 className="-projectcad-head animation-container">Resume</h1>
-                <p className="-projectcad-pera animation-container "> Full-stack e-commerce solution with payment integration, inventory management, and admin dashboard.</p>
+                <p className="-projectcad-pera animation-container ">  Interactive resume builder using React to create, preview, edit, and download professional resumes easily.</p>
                 <div className="poject-button-container animation-container">
                    <a className="projectcad-giigub-button animation-container"
                     href="https://github.com/venkatesh3553/resumes.git" target="_blank"
@@ -62,6 +62,76 @@ const Projects = ()=>(
                     >Live Demo</a>
                 </div>
                 
+            </div>
+             <div className="project-card animation-div">
+                    <img src="https://opengameart.org/sites/default/files/preview_937.png" alt="Image"
+                    className="project-img"/>
+                    <h1 className="-projectcad-head animation-container">Python Snake Game</h1>
+                    <p className="-projectcad-pera animation-container "> Classic Snake game built in Python using Pygame with smooth controls and real-time gameplay.</p>
+                    <div className="poject-button-container animation-container">
+                    <a className="projectcad-giigub-button animation-container"
+                        href="https://github.com/venkatesh3553/Python-Snake-Game.git" target="_blank"
+                        >GitHub</a>
+                        {/* <a className="projectcad-livedemo-button animation-container"
+                        href="https://resumes-ebon.vercel.app/" target="_blank"
+                        >Live Demo</a> */}
+                    </div>
+            </div>
+            <div className="project-card animation-div">
+                    <img src="https://static.vecteezy.com/system/resources/previews/000/583/157/original/sound-wave-ilustration-logo-vector-icon-template.jpg" alt="Image"
+                    className="project-img"/>
+                    <h1 className="-projectcad-head animation-container">Python Audio</h1>
+                    <p className="-projectcad-pera animation-container "> A simple Python audio player using Tkinter and Pygame, allowing users to play.</p>
+                    <div className="poject-button-container animation-container">
+                    <a className="projectcad-giigub-button animation-container"
+                        href="https://github.com/venkatesh3553/Python-Audio.git" target="_blank"
+                        >GitHub</a>
+                        {/* <a className="projectcad-livedemo-button animation-container"
+                        href="https://resumes-ebon.vercel.app/" target="_blank"
+                        >Live Demo</a> */}
+                    </div>
+            </div>
+            <div className="project-card animation-div">
+                    <img src="https://tse1.mm.bing.net/th/id/OIP.eCsuVTLtK13xCuzPvekWGQHaHa?rs=1&pid=ImgDetMain&o=7&rm=3" alt="Image"
+                    className="project-img"/>
+                    <h1 className="-projectcad-head animation-container">Python Qr Generatar</h1>
+                    <p className="-projectcad-pera animation-container "> Python tool using `qrcode` to generate QR codes from text or URLs quickly.</p>
+                    <div className="poject-button-container animation-container">
+                    <a className="projectcad-giigub-button animation-container"
+                        href="https://github.com/venkatesh3553/Python-Qr-Generatar.git" target="_blank"
+                        >GitHub</a>
+                        {/* <a className="projectcad-livedemo-button animation-container"
+                        href="https://resumes-ebon.vercel.app/" target="_blank"
+                        >Live Demo</a> */}
+                    </div>
+            </div>
+            <div className="project-card animation-div">
+                    <img src="https://img.freepik.com/premium-vector/quiz-logo-poll-questionnaire-icon-symbol_101884-1076.jpg?w=2000" alt="Image"
+                    className="project-img"/>
+                    <h1 className="-projectcad-head animation-container">Quiz App</h1>
+                    <p className="-projectcad-pera animation-container ">Full-stack Quiz App with real-time scoring, dynamic questions, timer, and admin question management.</p>
+                    <div className="poject-button-container animation-container">
+                    <a className="projectcad-giigub-button animation-container"
+                        href="https://github.com/venkatesh3553/FullStack-Quiz-App.git" target="_blank"
+                        >GitHub</a>
+                        <a className="projectcad-livedemo-button animation-container"
+                        href="https://full-stack-quiz-app-ten.vercel.app/" target="_blank"
+                        >Live Demo</a>
+                    </div>
+            </div>
+             <div className="project-card animation-div">
+                    <img src="https://png.pngtree.com/background/20231030/original/pngtree-3d-render-of-a-food-delivery-application-picture-image_5803466.jpg" alt="Image"
+                    className="project-img"/>
+                    <h1 className="-projectcad-head animation-container">Food Order App</h1>
+                    <p className="-projectcad-pera animation-container "> Full-stack food ordering app with menu browsing, cart management, order tracking, and admin control.</p>
+                    <div className="poject-button-container animation-container">
+                    <a className="projectcad-giigub-button animation-container"
+                        href="https://github.com/venkatesh3553/My-Food-App.git" target="_blank"
+                        >GitHub</a>
+                        <a className="projectcad-livedemo-button animation-container"
+                        href="https://my-food-app-ten.vercel.app/" target="_blank"
+                        >Live Demo</a>
+                    </div>
             </div>
 
         </div>
